14c4013145d10c9e32074dff58ed87f41cc6878c
Jokegom <a.bagmat2005@gmail.com>
2024-07-14 16:06:39 +0300

    add pep 8 enter, and f string

--- firstTask.py
+++ firstTask.py
@@ -8,6 +8,6 @@ input_c = input("Введите число b: ")
 c = int(input_c)
 D = (pow(b, 2) - 4 * (a * c))
 D_sqrt = (D ** 0.5)
-x1 = (-b + D_sqrt / 2 * a)
-x2 = (-b - D_sqrt / 2 * a)
+x1 = f"(-b + {D_sqrt} / 2 * a)"
+x2 = f"(-b - {D_sqrt} / 2 * a)"
 print("x1 =", x1, "x2 =", x2)
